cad577807599b5636d73e7bcbe12306084acdb8d
LordVareX <42437839+LordVareX@users.noreply.github.com>
2022-05-10 16:32:43 +0800

    update character asset

---
+++ Source/LyraGame/Inventory/InventoryFragmentPickupScrapIcon.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Inventory/InventoryFragmentPickupScrapIcon.h"
+
+UInventoryFragmentPickupScrapIcon::UInventoryFragmentPickupScrapIcon()
+{
+	PadColor = FLinearColor::Green;
+}
